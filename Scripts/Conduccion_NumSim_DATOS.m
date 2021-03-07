ffb687658abaf86befb5f837f8127dfd35c6801c
Diego <diego.mataix@hotmail.com>
2021-03-07 23:33:48 +0100

    ]]]

--- Scripts/Conduccion_NumSim_DATOS.m
+++ Scripts/Conduccion_NumSim_DATOS.m
@@ -10,35 +10,37 @@ close all; clear all; clc;
 
 %% Datos
 %%%% PCB (de FR4) %%%%
-dx = 140e-3;    % m
-dy = 100e-3;    % m
-dz = 1.5e-3;    % m
-A = dx*dy;      % m^2
-Vol = dx*dy*dz; % m^3
+dx = 140e-3;    % [m]
+dy = 100e-3;    % [m]
+dz = 1.5e-3;    % [m]
+A = dx*dy;      % [m^2]
+Vol = dx*dy*dz; % [m^3]
     % Recubrimiento de cobre del PCB por cada lado
-t_rec = 50e-6;  % m     % en una de las caras es continuo, y en la otra 
+t_rec = 50e-6;  % [m]     % en una de las caras es continuo, y en la otra 
                         % ocupa solo el 10% de la superficie en la cual van 
                         % montados tres IC
     % los lados cortos de la PCB tienen contacto termico con paredes a 25C
 T_b =  convtemp(25, 'C', 'K'); 
     % los otros dos bordes estan termicamente aislados.
     % para el FR4:
-k_plano = 0.5;              % W / ( m * K )
-k_traves = k_plano / 2;     % W / ( m * K )
+k_plano = 0.5;              % [W / ( m * K )]
+k_traves = k_plano / 2;     % [W / ( m * K )]
                         
 %%%% IC %%%%
-dx_ic = 40e-3;  % m
-dy_ic = 20e-3;  % m
-dz_ic = 3e-3;   % m
-Vol_ic = dx_ic * dy_ic * dz_ic; % m^3
+dx_ic = 40e-3;  % [m]
+dy_ic = 20e-3;  % [m]
+dz_ic = 3e-3;   % [m]
+Vol_ic = dx_ic * dy_ic * dz_ic; % [m^3]
     % dispando:
-Q_ic = 5;   % W
+Q_ic = 5;       % [W]
+Q_ic_tot = 3 * Q_ic /2;     % [W] (symmetry)
     % conductividad termica:
-k_ic = 50;      % W / ( m * K )
+k_ic = 50;       % [W / ( m * K )]
+k_Cu = 386;      % [W / ( m * K )]
     % capacidad termica:
-C_ic = 20;      % J / K
+C_ic = 20;       % [J / K]
     % separación entre ICs
-dist_ic = 20e-3;% m 
+dist_ic = 20e-3;% [m] 
 
 %%%% Otros datos %%%%
 % calor transmitido por radiacion:
@@ -49,14 +51,14 @@ T_caja = convtemp(45, 'C', 'K'); % T caja electronica (para apartado c)
 
 %% Constantes
 
-h = 6.6256e-34;  % J * s                      % Plank's constant
-c_0 = 2.9979e8;  % m / s                      % velocidad de la luz
-stefan_boltz = 5.67e-8; % W / ( m^2 * K^4 )   % Stefan-Boltzmann constant
-T_sun = 5800;    % K
-T_cbr = 2.7;     % K
-C_1 = 3.742e8;   % w * micrometer^4 / m^2  % 2 * pi * h * c^2
-C_2 = 1.439e4;   % micrometer * K          % h * c / k
-k = 1.38065e-23; % J / K                      % Boltzmann constant
-visible_range_min = 0.4e-6;  % m  % bandwidth
-visible_range_max = 0.76e-6; % m  % bandwidth   
+h = 6.6256e-34;  % [J * s]                      % Plank's constant
+c_0 = 2.9979e8;  % [m / s]                      % velocidad de la luz
+stefan_boltz = 5.67e-8; % [W / ( m^2 * K^4 )]   % Stefan-Boltzmann constant
+T_sun = 5800;    % [K]
+T_cbr = 2.7;     % [K]
+C_1 = 3.742e8;   % [w * micrometer^4 / m^2      % 2 * pi * h * c^2]
+C_2 = 1.439e4;   % [micrometer * K]             % h * c / k
+k = 1.38065e-23; % [J / K]                      % Boltzmann constant
+visible_range_min = 0.4e-6;  % [m]  % bandwidth
+visible_range_max = 0.76e-6; % [m]  % bandwidth   
 
---
+++ Scripts/simulacion_numerica_del_proceso_transitorio_1D.m
@@ -0,0 +1,57 @@
+%% 'HT_1D_planar.m'. 
+
+%One-dimensional heat transfer in planar geometry by finite differences. 
+%Equation to solve: rho*c*DT/Dt=k*D2T/Dx2+phi-rho*c*v*DT/Dx
+
+clear;close all;            %Default data applicable to Problem P-11.53
+Conduccion_NumSim_DATOS
+Conduccion_NumSim_v0
+
+%% Data:        %Change data and see corresponding results
+D=dx;           %Diametre or characteristic transversal dimension [m]
+L=dy;           %Length along heat path [m]
+A=pi*D^2/4;     %Area. Direct input, or computed from diameter as A=pi*D^2/4 [m2]
+p=pi*D;         %Perimeter. Direct input, or computed from diameter as p=pi*D [m]
+k=k_eff;        %Conductivity [W/(m·K)]
+c=C_ic;           %Thermal capacity [J/(kg·K)]
+rho=1000;       %Density [kg/m3]
+h=2;            %Convective coefficient [W/(m^2·K)], transversal
+eps=0;          %Emissivity, transversal
+sigma=5.67e-8;  %S-B constant
+N=20;           %# of elements along L
+M=1e5;          %# of time steps
+Tinf=288;       %Ambient temperature [K]
+tsim=600;     %Total simulation time [s]
+Troot=303;      %Root temperature [K] in case it is fixed
+Qroot=0;        %Root heat transfer power [W] in case it is fixed
+phi=5/(A*L);    %Volumetric dissipation [W/m^3] if any
+
+%%Inicialization:
+a=k/(rho*c);            %Diffusivity [m^2/s]
+Dx=L/N;                 %Element width
+X=linspace(0,L,N+1);    %Node position list (equispaced)
+Dt=tsim/M;              %Time step (you might fix it instead of tsim)
+t=linspace(0,tsim,M)';  %Time vector
+Fo=a*Dt/(Dx*Dx)         %Fourier's number
+Bi=h*p*Dx/(k*A/Dx)      %Biot's number
+%Check for stability of the explicit finite difference method
+disp(['Stability requires 1-Fo*(2+Bi)<0. It actually is =',num2str(1-Fo*(2+Bi))])
+if 1-Fo*(2+Bi)<0 disp('This is unstable; increase number of time steps'), end
+T=Tinf*ones(M,N+1); 	%Temperature-matrix (times from 1 to M, and positions from 1 to N+1)
+ 
+%Iteration:   
+for it=2:M
+    for i=2:N
+        T(it,i)=T(it-1,i)+Fo*(T(it-1,i+1)-2*T(it-1,i)+T(it-1,i-1))+Fo*Bi*(Tinf-T(it-1,i))+phi*Dt/(rho*c);
+    end
+    %Boundory condition in node 0 (one of the following two):
+    T(it,1)=Troot;      %if Troot is fixed
+    %T(it,1)=T(it,2)-Qroot*Dx/(2*k*A);   %if Qroot is fixed
+    %Boundory condition in node N (one of the following two):
+    T(it,N+1)=Troot;    %if Troot is fixed
+    %T(it,N+1)=T(it,N)-Qroot*Dx/(2*k*A);   %if Qroot is fixed
+end
+ 
+%Presentation of results
+subplot(2,1,1);plot(t,T(:,1:N/10:N+1));xlabel('t [s]'),ylabel('T [K]');title('T(t,x) vs. t at several locations')
+subplot(2,1,2);plot(X,T(1:M/100:M,:));xlabel('X [m]'),ylabel('T [K]');title('T(t,x) vs. X at several times')
